9d582a7214ca95b4d838490b61d960dd61324b3f
Linnea Ingmar <linnea.ingmar@hotmail.com>
2017-11-18 15:50:11 +0100

    Add possibility to split data into months

--- bankifyr/statistics.py
+++ bankifyr/statistics.py
@@ -45,20 +45,64 @@ def to_number(s):
     return float(s.replace(" ", "").replace(",", "."))
 
 
-def histogram(data):
+def histogram(data, title):
     keys = list(data.keys())
     values = [-v for v in data.values()]
     sorted_values, sorted_keys = zip(*sorted(zip(values, keys)))
-    
-    with PdfPages("foo.pdf") as pp:
-        ones = [1 for _ in range(1, len(sorted_keys) + 1)]
-        plt.bar(range(len(sorted_keys)), sorted_values)
-        plt.xticks(range(len(sorted_keys)), sorted_keys, rotation=50)
-        pp.savefig()
 
+    plt.clf()
+    plt.bar(range(len(sorted_keys)), sorted_values)
+    plt.xticks(range(len(sorted_keys)), sorted_keys, rotation=50)
+
+
+def split_months(rows):
+    months = set()
     
+    for row in rows:
+        months.add(row[1].month)
+
+    for month in months:
+        histogram(count_categories(filter_month(rows, month)),
+                  month_name(month))
+        plt.savefig(month_name(month) + ".png")
+        
+        
+def month_name(n):
+    if n is 1:
+       return "January"
+    elif n is 2:
+       return "February"
+    elif n is 3:
+       return "March"
+    elif n is 4:
+       return "April"
+    elif n is 5:
+       return "May"
+    elif n is 6:
+       return "June"
+    elif n is 7:
+       return "July"
+    elif n is 8:
+       return "August"
+    elif n is 9:
+       return "September"
+    elif n is 10:
+       return "October"
+    elif n is 11:
+       return "November"
+    elif n is 12:
+       return "December"
+
+        
+def filter_month(rows, month):
+    result = []
+    for row in rows:
+        if row[1].month == month:
+            result.append(row)
+    return result
+            
+
 if __name__ == '__main__':
     filename = sys.argv[1]
     rows = read_rows(filename)
-    cat_count = count_categories(rows)
-    histogram(cat_count)
+    split_months(list(rows))
